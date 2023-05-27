4737baae3d49e58de2fb59e114736ec4a62c054b
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2023-05-28 01:10:39 +0900

    [.run.sh] Added a brief documentation.

--- .run.sh
+++ .run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
 ####################################################
 ## Stand-alone example run script
 ## Murilo M. Marinho (www.murilomarinho.info)
 ####################################################
+# If you run without this script and it doesn't work
+# you'll understand why this script was made in the
+# first place :0
 
 LD_LIBRARY_PATH=lib bin/adaptive_control_example
