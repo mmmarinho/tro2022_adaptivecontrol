a3a1656186a3bedf85a68b8e7b336d328934ac99
Murilo Marinho <murilomarinho@manchester.ac.uk>
2025-05-14 13:51:22 +0100

    Removing dist-utils as it might have been hogging up the other install

--- test_python_wrapper.sh
+++ test_python_wrapper.sh
@@ -6,7 +6,7 @@ if [ "$(uname)" == "Darwin" ]; then
     brew install eigen cppzmq boost
 elif [ "$(expr substr "$(uname -s)" 1 5)" == "Linux" ]; then
     echo "Installing prerequisites with apt..."
-    sudo apt install python3-dev python3-distutils libeigen3-dev libzmq3-dev libboost-all-dev
+    sudo apt install python3-dev libeigen3-dev libzmq3-dev libboost-all-dev
 else
     echo "Unrecognized system."
 fi
